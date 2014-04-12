14c811e3a83e1ff7414f36eeec45e82b3c981fed
xhanin <xavier.hanin@4sh.fr>
2014-04-12 14:08:35 +0200

    implement add event on client

--- ui/app/scripts/controllers/events.js
+++ ui/app/scripts/controllers/events.js
@@ -2,5 +2,13 @@
 
 angular.module('iwasthereApp')
   .controller('EventsCtrl', function ($scope, Event) {
-    $scope.events = Event.query();
+        $scope.events = Event.query();
+
+        $scope.addEvent = function() {
+            var ev = new Event($scope.event);
+            ev.$save(function(ev) {
+                $scope.events.push(ev);
+                $scope.showAddEvent = false;
+            });
+        }
   });
